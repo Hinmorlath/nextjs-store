311b499b60121cdbc5635777b42a015646a588e1
Hinmorlath <josepolomanuel2@gmail.com>
2024-03-11 16:56:54 -0600

    2_4 Archivos estáticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,11 @@
 export const Description = () => {
     return (
         <section>
-            <h1>Description</h1>
+            <img src="/images/Vivotaku 2.png" alt="Vivotaku Logo" />
+
+            <h2>Description</h2>
+
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi natus id expedita accusamus quidem animi veniam doloribus recusandae ea, blanditiis quos saepe, praesentium voluptate, minima perferendis magni necessitatibus deserunt. Officiis!</p>
         </section>
     )
 }
