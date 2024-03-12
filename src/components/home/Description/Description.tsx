84939660223d57ce8f59757ba8ef5ca6b21adc64
Hinmorlath <josepolomanuel2@gmail.com>
2024-03-12 09:51:54 -0600

    2_5 Manejo y optimizacion de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,11 +1,21 @@
+import styles from "./Description.module.sass";
+import Image from "next/image";
+
 export const Description = () => {
     return (
-        <section>
-            <img src="/images/Vivotaku 2.png" alt="Vivotaku Logo" />
+        <section className={styles.Description}>
+            <Image
+                src="/images/Vivotaku 2.png" 
+                alt="Vivotaku Logo" 
+                height={300} 
+                width={300}
+            />
+            
+            <div className={styles.Description__text}>
+                <h2>Description</h2>
 
-            <h2>Description</h2>
-
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi natus id expedita accusamus quidem animi veniam doloribus recusandae ea, blanditiis quos saepe, praesentium voluptate, minima perferendis magni necessitatibus deserunt. Officiis!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi natus id expedita accusamus quidem animi veniam doloribus recusandae ea, blanditiis quos saepe, praesentium voluptate, minima perferendis magni necessitatibus deserunt. Officiis!</p>
+            </div>
         </section>
     )
 }
